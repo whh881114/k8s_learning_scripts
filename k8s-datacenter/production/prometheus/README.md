15ec52bd2cb1475e955267a0f5c259b309868b87
wanghaohao <wanghaohao@pagoda.com.cn>
2021-01-02 00:06:48 +0800

    重新编排

---
+++ k8s-datacenter/production/prometheus/README.md
@@ -0,0 +1,13 @@
+# 说明
+- prometheus
+    - 从kubernetes-1.16.15官方源码文件中找到的，然后根据实际情况，自己修改。地址为：https://github.com/kubernetes/kubernetes/tree/release-1.16/cluster/addons/prometheus。
+
+- alertmanager
+    - 没有部署。
+
+- kube-state-metrics
+    - https://github.com/kubernetes/kube-state-metrics/tree/master/examples/standard
+    
+- node-exporter
+
+- mysqld-exporter
---
+++ k8s-datacenter/production/prometheus/mysqld-exporter/generate_pods.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+namespace=db-drill
+db_pods=`kubectl get pods -n $namespace | grep -v '^NAME' | awk '{print $1}'`
+
+for db_pod in $db_pods
+do
+  sed -e "s/mysqld-exporter-sample/mysqld-exporter-$db_pod/g" -e "s/replace_mysql_inst_here/${db_pod/-0/}-raw.$namespace.svc.cluster.local/g" sample.yaml >  $db_pod.yaml
+done
+
+db_pod_svcs=`kubectl get svc -n production-prometheus | grep -v 'NAME' | awk '{print $1}'`
+for db_pod_svc in $db_pod_svcs
+do
+  echo "- $db_pod_svc.production-prometheus.svc.cluster.local:9104"
+done
+
+
+# 需要在db中创建一个授权账号。
+#db_instances='10.43.147.238
+#10.43.239.38
+#10.43.231.114
+#10.43.250.166
+#10.43.115.138
+#10.43.113.198
+#10.43.66.126
+#10.43.21.185
+#10.43.60.114
+#10.43.48.41
+#10.43.238.191
+#10.43.31.122
+#10.43.252.121
+#10.43.238.90
+#10.43.164.11
+#10.43.29.24
+#10.43.165.20
+#10.43.222.1
+#10.43.158.144'
+#
+#for db in $db_instances
+#do
+#  mysql -h $db -uroot -p"MyPassword007." -e "GRANT PROCESS, REPLICATION CLIENT ON *.* TO 'dbcheck'@'10.%';"
+#  mysql -h $db -uroot -p"MyPassword007." -e "GRANT SELECT ON performance_schema.* TO 'dbcheck'@'10.%';"
+#  mysql -h $db -uroot -p"MyPassword007." -e "FLUSH PRIVILEGES;"
+#done
