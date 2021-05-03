909325974f238b8fcfb28b88c94bd0f89df88cc5
wanghaohao <wanghaohao@pagoda.com.cn>
2021-05-03 20:32:37 +0800

    更新redis-cluster实例common配置

--- datacenter/redis-cluster/common/create_cluster.sh
+++ datacenter/redis-cluster/common/create_cluster.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 NAMESPACE=redis-cluster
-PODNAME=redis-cluster-common
+PODNAME=common
 
-kubectl -n $NAMESPACE exec ${PODNAME}-0 -it -- redis-cli -a "absebfz2rer@hbseylpySx6dlQczdylv" --cluster create --cluster-replicas 1 $(kubectl -n $NAMESPACE get pods -l app=$PODNAME -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
+# k8s版本v1.19前。
+# kubectl -n $NAMESPACE exec ${PODNAME}-0 -it -- redis-cli -a "absebfz2rer@hbseylpySx6dlQczdylv" --cluster create --cluster-replicas 1 $(kubectl -n $NAMESPACE get pods -l app=$PODNAME -o jsonpath='{range.items[*]}{.status.podIP}:6379 ')
+
+# k8s版本v1.19。
+kubectl -n $NAMESPACE exec ${PODNAME}-0 -it -- redis-cli -a "absebfz2rer@hbseylpySx6dlQczdylv" --cluster create --cluster-replicas 1 $(kubectl -n $NAMESPACE get pods -l app=$PODNAME -o jsonpath='{range.items[*]}{.status.podIP}:6379 ' | sed s/\ :6379\ $//)
