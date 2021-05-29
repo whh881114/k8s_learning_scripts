679232988b00c7dd60a4bf2a465641af5ee91be3
wanghaohao <wanghaohao@pagoda.com.cn>
2021-05-29 14:49:05 +0800

    zabbix\agent：dockerfile中指定root用户运行。

--- dockerfiles/zabbix-agent-centos7/build.sh
+++ dockerfiles/zabbix-agent-centos7/build.sh
@@ -2,7 +2,7 @@
 
 registry_url="harbor.freedom.org"
 registry_project="roy"
-image="$registry_url"/"$registry_project"/zabbix-agent:latest
+image="$registry_url"/"$registry_project"/zabbix-agent:4.0-centos-latest
 
 docker build -t $image .
 docker push $image
