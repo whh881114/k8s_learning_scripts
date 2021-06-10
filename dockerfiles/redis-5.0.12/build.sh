923af128218f6c33ca82478ae44994bdf2542a6b
Roy Wong <whh881114@gmail.com>
2021-06-10 23:34:02 +0800

    redis-5.0.12将镜像地址换成hub.docker.com

--- dockerfiles/redis-5.0.12/build.sh
+++ dockerfiles/redis-5.0.12/build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-registry_url="hub.docker.com"
-registry_project="whh881114"
-image="$registry_url"/"$registry_project"/redis:5.0.12
+# 当使用hub.docker.com镜像仓库时，其镜像名字前不用加此地址。
+#registry_url="hub.docker.com"
+
+docker_id="whh881114"
+image="$docker_id"/redis:5.0.12
 
 docker build -t $image .
 docker push $image
